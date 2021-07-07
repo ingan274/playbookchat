499f82900d1b138273dad89973438324cd9bc6c9
ingan <isabelngan27@gmail.com>
2021-07-07 10:22:18 -0400

    adjusting cors

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const path = require('path');
 const express = require("express");
 const router = require("./router/index");
-const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
@@ -13,7 +12,6 @@ const PORT = process.env.PORT || 3002;
 // // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
 app.use(morgan('combined'));
 // // Add routes - this connects and activates API
 app.use(router);
