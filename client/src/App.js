ef5d46c0c362fd0457c8bd325f5db5e0f56be1ea
ingan <isabelngan27@gmail.com>
2021-07-12 11:11:20 -0400

    adjusting app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import MCCCrew from "./MCCCrew";
 import Crew from "./Crew";
-import IVview from "./MCCandCrew";
 import UserSelect from "./User Selection";
 import {
   BrowserRouter as Router,
@@ -22,9 +21,6 @@ function App() {
         </Route>
         <Route exact path="/playbookcrew">
           <Crew />
-        </Route>
-        <Route exact path="/playbookivview">
-          <IVview />
         </Route>
       </Switch>
     </Router>
