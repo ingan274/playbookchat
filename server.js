5a5d008cca820a16742986f1ec2faeb127d19d03
ingan <isabelngan27@gmail.com>
2021-07-18 16:38:01 -0400

    please work

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.use(router);
 if (process.env.NODE_ENV === 'production') {
     // console.log('YOU ARE IN THE PRODUCTION ENV');
     app.use('/static', express.static(path.join(__dirname, './client/build/static')));
+    app.use(express.static(path.join(__dirname, '../build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../build'))
+    })
 }
 // // Start the API server
 app.listen(PORT, function () {
