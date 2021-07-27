85622c71a168487060b8699aa51607c37731657f
Isabel Ngan <49883486+ingan274@users.noreply.github.com>
2021-07-27 13:13:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,24 +13,20 @@ const PORT = process.env.PORT || 3002;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(morgan('combined'));
-// // Add routes - this connects and activates API
-app.use(router);
-// app.use(express.static(path.join(__dirname, './client/public')));
 
 // // If its production environment!
 if (process.env.NODE_ENV === 'production') {
-    // console.log('YOU ARE IN THE PRODUCTION ENV');
+    console.log('YOU ARE IN THE PRODUCTION ENV');
     // app.use('/static', express.static(path.join(__dirname, '../client/build')));
     app.use(express.static(path.join(__dirname, './client/build')))
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, './client/build/index.html'))
     })
 }
+
+app.use(router);
 // // Start the API server
 app.listen(PORT, function () {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-
 
 // RUN THIS IN NGROK
 // ./ngrok http 3000 -host-header="localhost:3000"
